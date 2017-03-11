b4754bbfe79ad9094190b6c9b4c29f9a5ae1c078
Lewek, Jakub <jakub.lewek@contour-technology.com>
2017-03-11 17:35:10 +0100

    home item list directive

---
+++ client/app/main/mainItemList.directive.js
@@ -0,0 +1,50 @@
+import angular from 'angular';
+import InventoryModel from '../item-management/im.service';
+
+export default function ItemList () { 
+    return {
+      controller: ['$scope', 'InventoryModel',  function ($scope,InventoryModel) {
+        //TODO: get data from service and remove mock
+        /*InventoryModel.getAll().then(function(itemData){
+            $scope.itemData = itemData;
+        });*/
+
+        // data mock
+        var itemData = [];
+        for(var i=0;i<20;i++) {
+          itemData.push({
+            name: 'Test',
+            description: 'test abc',
+            price: {
+                buy: 43,
+                actual: 11,
+                sell: 43
+            },
+            inUse: true,
+            broken: true,
+            documents: [],
+            created: Date.now(),
+            _canBeRemoved: true,
+            img: [],
+            room: '12',
+            name: 'Test',
+            description: 'test abc',
+            price: {
+                buy: 43,
+                actual: 11,
+                sell: 43
+            },
+            inUse: true,
+            broken: true,
+            documents: [],
+            created: Date.now(),
+            _canBeRemoved: true,
+            img: [],
+            room: '12'
+          });
+        }
+        $scope.itemData = itemData;
+      }],
+      template: require( './mainItemList.html')
+    }
+};
